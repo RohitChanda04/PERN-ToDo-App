b100bb14a7b89310060ed46ca75dbf23f79b8179
Rohit Chanda <rohit.chanda4@gmail.com>
2025-01-10 13:30:39 -0800

    Added modal for delete functionality and changed UI theme for buttons

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import EditTodo from './EditTodo';
+import DeleteTodo from './DeleteTodo';
 
 const ListTodos = () => {
     // to capture the array of todos
@@ -11,21 +12,6 @@ const ListTodos = () => {
             const response = await fetch("http://localhost:3000/todos");
             const data = await response.json();
             setTodos(data);
-        }
-        catch (error) {
-            console.log(error.message);
-        }
-    }
-
-    // function to delete a specified todo
-    const deleteTodo = async (todo_id) => {
-        try {
-            // eslint-disable-next-line
-            const response = await fetch(`http://localhost:3000/todos/delete-todo/${todo_id}`, {
-                method: "DELETE"
-            });
-
-            setTodos(todos.filter(todo => todo.todo_id !== todo_id));
         }
         catch (error) {
             console.log(error.message);
@@ -60,8 +46,7 @@ const ListTodos = () => {
                                     <td>{todo.todo_id}</td>
                                     <td>{todo.description}</td>
                                     <td><EditTodo todo = {todo} /></td>
-                                    <td><button className='btn btn-danger'
-                                    onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
+                                    <td><DeleteTodo todo = {todo} /></td>
                                 </tr>
                             ))}
                         </tbody>
